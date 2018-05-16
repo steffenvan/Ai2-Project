7f23f3e0655bda8742da6c18e2639a57d5feefad
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-05-16 10:39:51 +0200

    first working version of the preprocessing programm

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,23 +1,14 @@
 source = open("article.xml",'r')
 target = open("out.txt",'w')
 c = source.read(1)
+
+
 while c :
     if (c=='<') :              # skip the parts between <> for now
         while (c!='>') :
             c = source.read(1)
         c = source.read(1)
     else :
-        if (c!='-') :
-            target.write(c)
-            c = source.read(1)
-        else :                      # two cases :
-            d = source.read(1)
-            if (d!='\n') :          # 1) hyphenated terms
-                target.write(c)
-                target.write(d)
-                c = source.read(1)
-            else :                  # 2) end of line hyphenation
-                c = source.read(1)
 
         if (c=='-') :
             d = source.read(1)
@@ -27,9 +18,27 @@ while c :
                 c = source.read(1)
             else :                  # 2) end of line hyphenation
                 c = source.read(1)
-        elif(c=='&') :
 
-        else :       # regular characters
+        elif(c=='&') :              # special xml characters
+            next2 = source.read(2)
+            if (next2 == 'lt') :
+                target.write('<')
+            elif (next2 == 'gt') :
+                target.write('>')
+            elif (next2 == 'am') :
+                target.write('&')
+            elif (next2 == 'ap') :
+                target.write("\'")
+            else :
+                target.write("\"")
+
+            c = source.read(1)
+
+            while (c!=";") :
+                c = source.read(1)
+            c = source.read(1)
+
+        else :       # finally, regular characters are added to target
             target.write(c)
             c = source.read(1)
 
@@ -45,19 +54,3 @@ Find a way to handle tags, maybe by creating a list of all important
 tags that would give corresponding parts of the text a certain
 weight.
 """
-
-# source = open("article.xml",'r')
-#
-# c = source.read(1)
-#
-# s = ""
-#
-# while(c) :
-#     if (c=='-') :
-#         c = source.read(1)
-#         if (c=='\n') :
-#             c = source.read(1)
-#     s+=c
-#     c = source.read(1)
-#
-# print(s)
