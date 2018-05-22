426c7626693a18bbe52e4392fd341a3b85778f1c
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-05-22 11:10:24 +0200

    added the final preprocessing pipeline, with instructions to follow in instructions_preprocess.txt

--- src/display.py
+++ src/display.py
@@ -4,21 +4,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-noisy_pos_tags = ["IN"]
+df = pd.read_pickle("../data/similarities.pkl")
 
-nlp = spacy.load("en")
-
-doc = nlp(open("out.txt",'r').read())
-
-words = [str(doc[i]).lower() for i in range(len(doc))]
-
-pos = [doc[i].pos_ for i in range(len(doc))]
-
-
-d = {'words' : words, 'pos' : pos}
-
-df = pd.DataFrame(data = d)
-
-sns.countplot(df.pos)
+sns.heatmap(df, annot = True)
 
 plt.show()
--- src/quick.py
+++ src/quick.py
@@ -1,3 +1,1 @@
-from preprocess import preprocess, remove_empty_lines
-
-remove_empty_lines("../data/A00_txt/A00-1000-parscit.130908.txt")
+print(2*"s")
