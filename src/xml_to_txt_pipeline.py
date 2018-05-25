3968335b3541c4a782631db0166f5a1dc183fc7f
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-05-25 11:57:04 +0200

    toy data set processed at /data/txt/

--- src/xml_to_txt_pipeline.py
+++ src/xml_to_txt_pipeline.py
@@ -1,4 +1,4 @@
-from preprocess import preprocess, remove_empty_lines
+from preprocess import preprocess, remove_empty_lines, one_sentence_per_line
 from create_df import create_df
 from compare import compare
 import pandas as pd
@@ -6,28 +6,28 @@ import os
 import spacy
 import numpy as np
 
+use_df = 0        # not using the comparision dataframe for now, just converting xml to txt
+
+
 xml_path = "../data/xml/"
 txt_path = "../data/txt/"
 new_path = "../data/new_xml/"
 
+def process_new_xml() :
+    new_files = os.listdir(new_path)
+    num_new_files = len([elt for elt in new_files if elt.endswith('.xml')])
 
-nlp = spacy.load('en')
-
-
-if (os.path.exists("../data/similarities.pkl")) :
-
-    sim = pd.read_pickle("../data/similarities.pkl")
-
-else :
-
-    create_df()
-
-    sim = pd.read_pickle("../data/similarities.pkl")
+    for new_filename in new_files:
+        if (new_filename.endswith(".xml") and (new_filename not in os.listdir(xml_path))) :  # check if a new file isn't already in the data base
+            preprocess(new_path + new_filename, txt_path + new_filename[:-4] + ".txt" )
+            remove_empty_lines(txt_path + new_filename[:-4] + ".txt")
+            one_sentence_per_line(txt_path + new_filename[:-4] + ".txt")
+            os.rename(new_path + new_filename, xml_path + new_filename)   # moves the initial .xml file from the new_xml to the xml folder
 
 
 
 
-def process_new_xml(old_df) :    # preprocesses all the .xml files in the new_xml folder
+def process_new_xml_with_df(old_df) :    # preprocesses all the .xml files in the new_xml folder
 
     M = old_df.as_matrix()   # Matrix from the old dataFrame
     n = M.shape[1]
@@ -53,6 +53,9 @@ def process_new_xml(old_df) :    # preprocesses all the .xml files in the new_xm
                 remove_empty_lines(txt_path + new_filename[:-4] + ".txt")
                 os.rename(new_path + new_filename, xml_path + new_filename)   # moves the initial .xml file from the new_xml to the xml folder
 
+
+
+
                 new_doc = nlp(open(txt_path + new_filename[:-4] + ".txt").read())
 
 
@@ -69,6 +72,7 @@ def process_new_xml(old_df) :    # preprocesses all the .xml files in the new_xm
 
                 files.append(new_filename[:-4] + ".txt")
                 i += 1
+
 
 
         for i in range(num_new_files) :              # set the new diagonal terms to 1
@@ -88,9 +92,24 @@ def process_new_xml(old_df) :    # preprocesses all the .xml files in the new_xm
 
         return(sim)
 
+if (use_df) :
+    nlp = spacy.load('en')
 
 
-new_df = process_new_xml(sim)
+    if (os.path.exists("../data/similarities.pkl")) :
+
+        sim = pd.read_pickle("../data/similarities.pkl")
+
+    else :
+
+        create_df()
+
+        sim = pd.read_pickle("../data/similarities.pkl")
+
+    new_df = process_new_xml_with_df(sim)
+
+    new_df.to_pickle("../data/similarities.pkl")
 
 
-new_df.to_pickle("../data/similarities.pkl")
+else :
+    process_new_xml()
