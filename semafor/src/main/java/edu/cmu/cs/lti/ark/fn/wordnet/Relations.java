c7fa5db258c45b7f232d52bc724ef125d984948d
Steffen Van <steffenvan@Steffens-MacBook-Pro.local>
2018-06-05 14:31:32 +0200

    Removed the unecessary files from semafor

--- semafor/src/main/java/edu/cmu/cs/lti/ark/fn/wordnet/Relations.java
+++
@@ -1,8 +0,0 @@
-package edu.cmu.cs.lti.ark.fn.wordnet;
-
-import java.util.Set;
-
-/** Finds token relationships */
-public interface Relations {
-	public Set<String> getRelations(String actualTokens, String hiddenUnitTokens);
-}
