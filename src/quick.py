dfa94be0b5f0a2592148908371650674d043ec6f
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-06-07 11:58:48 +0200

    added first visulalisation tools for dependencies, with the networkx library

--- src/quick.py
+++ src/quick.py
@@ -1,63 +1,18 @@
-import spacy
-import os
 import json
-from random import shuffle
+import networkx as nx
+import matplotlib.pyplot as plt
+from spacy.lang.en import STOP_WORDS
+#from dependencies import extract_text
+import spacy
 
-nlp = spacy.load('en')
+G = nx.Graph()
 
-json_path = "../data/json/"
+G.add_edge("word1","word2")
 
-def display_frame_instances_with_pos(frame_name, nmax = 5) :    # prints all the instances of a given frame among the toy dataset
-
-    os.system("clear")
-
-    files = os.listdir(json_path)
-
-    shuffle(files)
-
-    i = 1
-
-    for filename in files :
-
-        if i > nmax :
-
-            break
-
-        if filename.endswith(".json") :
+edges = {("word1","word2") : "test"}
 
 
-            if i > nmax :
+nx.draw(G, with_labels = True)
+nx.draw_networkx_edge_labels(G, pos=nx.spring_layout(G), edge_labels = edges)
 
-                break
-
-
-
-            output = json.load(open(json_path + filename))
-
-            for sentence in output :
-
-                        if i > nmax :
-
-                            break
-
-                        for frame in sentence["frames"] :
-
-                            if i > nmax :
-
-                                break
-
-                            if frame["target"]["name"] == frame_name :
-
-                                curr_sentence = " ".join(sentence["tokens"])
-                                pos = [str(token.pos_) for token in nlp(str(curr_sentence))]
-                                string = ""
-                                for k in range(len(list(sentence["tokens"]))) :
-                                    string += (sentence["tokens"][k])
-                                    string += (' [' + pos[k] + '] ')
-
-                                print(string)
-                                print('\n')
-                                i += 1
-
-
-display_frame_instances_with_pos("Desirability", 5)
+plt.show()
