db094f097f3b1faffe22469a2fc943f67fa75ec3
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-06-20 16:06:04 +0200

    added several data analysis tools, as well as a txt files with all the frames sorted by number of appearance in the corpus

---
+++ src/explore.py
@@ -0,0 +1,77 @@
+"""
+This file contains functions useful for preliminary data analysis, such as extraction of frames or sentences containing certain words
+"""
+import os
+import json
+from path import root
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+json_path =  root + "data/json/"
+
+def number_of_frames(json_file) :
+    json_list = json.load(open(json_file))
+    number_of_frames = 0
+    
+    for i in range(len(json_list)) :
+        number_of_frames += len(json_list[i]["frames"])
+    
+    print("Total number of frames in the article : %d. Average number of frames per sentence : %d." %(number_of_frames,number_of_frames/len(json_list))) 
+    
+    return number_of_frames
+
+
+def number_of_sentences(json_file) :
+    json_list = json.load(open(json_file))
+    print("Total number of sentences in the article : %d." %(len(json_list)))
+    return number_of_sentences
+    
+
+# number_of_sentences(json_path + "E09-1005-parscit.130908-11.49.59.json")
+
+def count_frames(in_abstract = 0) :
+    
+    counts = {}
+    for filename in os.listdir(json_path) :
+        
+        if filename.endswith("json") :
+            if in_abstract :
+                doc = abstract_json(json_path + filename) 
+            else :
+                doc = json.load(open(json_path + filename)) 
+                
+            for sentence in doc :
+                for frame in sentence["frames"] :
+                    frame_name = frame["target"]["name"]
+                    if frame_name in counts :
+                        counts[frame_name] += 1
+                    else : 
+                        counts[frame_name] = 1            
+    return counts
+                    
+d = count_frames()
+
+tmp = []
+
+for key, value in d.items() :
+    tmptuple = (value, key)
+    tmp.append(tmptuple)
+
+tmp = sorted(tmp, reverse = True) 
+
+output = open("frames_counts.txt", 'w+')
+
+for elt in tmp :
+    output.write(str(elt) + '\n')
+    
+output.close()
+
+
+# print(head(df))
+
+# print(d)
+# 
+# print("total number of frames in the docs : %d" %(sum([val for val in d.values()])))
+# 
+# print("total number of frames in doc " + os.listdir(json_path)[2] + " : %d" %(number_of_frames(json_path + os.listdir(json_path)[2])))
