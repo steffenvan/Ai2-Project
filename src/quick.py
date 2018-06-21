e83e6bbc10ea5170224e8f35fd34175879d6b451
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-06-21 10:49:33 +0200

    modified quick.py

--- src/quick.py
+++ src/quick.py
@@ -1,76 +1,16 @@
-import torch
-import torch.nn as nn
-import torch.functional as F
-import torch.optim as optim
+import json
+from extraction import *
+from path import root
 
-torch.manual_seed(1)
+toy_path = root + 'data/toy_examples/'
 
-CONTEXT_SIZE = 2
-EMBEDDING_DIM = 10
+neg_file = "E09-1008-parscit.neg"
+frames_file = "E09-1008-parscit.130908-11.49.59.json"
 
-test_sentence = """When forty winters shall besiege thy brow,
-And dig deep trenches in thy beauty's field,
-Thy youth's proud livery so gazed on now,
-Will be a totter'd weed of small worth held:
-Then being asked, where all thy beauty lies,
-Where all the treasure of thy lusty days;
-To say, within thine own deep sunken eyes,
-Were an all-eating shame, and thriftless praise.
-How much more praise deserv'd thy beauty's use,
-If thou couldst answer 'This fair child of mine
-Shall sum my count, and make my old excuse,'
-Proving his beauty by succession thine!
-This were to be new made when thou art old,
-And see thy blood warm when thou feel'st it cold.""".split()
+neg = json.load(open(toy_path + neg_file))
+frames = json.load(open(toy_path + frames_file))
 
-trigrams = [[test_sentence[i],test_sentence[i+1],test_sentence[i+2]] for i in range(len(test_sentence)-2)]
-
-print(trigrams[:4])
-
-vocab = set(test_sentence)
-
-word_to_ix = {word : i for i, word in enumerate(vocab)}
-
-class NGramLanguageModeler(nn.Module) :
-    
-    def __init__(self, vocab_size, embedding_dim, context_size) :
-        super(NGramLanguageModeler, self).__init__()
-        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.linear1 = nn.Linear(context_size * embedding_dim, 128)     # hidden layer
-        self.linear2 = nn.Linear(128, vocab_size)                       # output layer
-        
-    
-    def forward(self, inputs) :
-        return slef
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+for i in range(len(neg_file)) :
+    print(neg[i])
+    print(frames[i])
+    print("*****************")
