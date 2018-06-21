a42ea61e13569fd6aedc82db77946e5587820e64
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-06-21 11:16:16 +0200

    modified quick.py

--- src/quick.py
+++ src/quick.py
@@ -4,13 +4,14 @@ from path import root
 
 toy_path = root + 'data/toy_examples/'
 
-neg_file = "E09-1008-parscit.neg"
-frames_file = "E09-1008-parscit.130908-11.49.59.json"
+neg_file = "E09-1005-parscit.json"
+frames_file = "E09-1005-parscit.130908-11.49.59.json"
 
 neg = json.load(open(toy_path + neg_file))
 frames = json.load(open(toy_path + frames_file))
 
-for i in range(len(neg_file)) :
+for i in range(len(neg)) :
     print(neg[i])
+    print("****")
     print(frames[i])
     print("*****************")
